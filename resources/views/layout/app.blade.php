aff0ccb10d65f840d85e2341c87c38207f871add
unknown <mindy80230@gmail.com>
2021-09-02 23:53:34 +0800

    重新拆區塊

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -11,9 +11,17 @@
 <link href="{{asset('t.css')}}" rel="stylesheet">
         <!-- Styles -->
         <style>
-            tr:nth-of-type(odd) td form .blocker{
+tr:nth-of-type(odd) td form .blocker{
     background: #6798C0;
     border-color: rgb(153, 214, 234);
+
+}
+
+body {　
+
+　overflow-y: scroll;
+
+　overflow-x: hidden;
 
 }
 tr:nth-of-type(even) td form .blocker{
@@ -38,33 +46,18 @@ background: black;
     left: 0%;
 }
         </style>
+             <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+             <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script> 
     </head>
-    <body class="antialiased"> 
-        <nav class="sticky-top navbar navbar-expand-lg navbar-light" role='navigation' style="background:#6798C0;">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-        </ul>
-        </nav>
-        <main>
+    <body class="antialiased">
+      
+          
         <div class="container-fluid">
             <div class="row flex-nowrap">
-                <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-1 bg-dark ">
-                    <div class="position-fixed text-white container">
-                        <a class="h2 text-white">Database</a>
-                       <ul class="nav navbar-nav text-white">
-                        <li ><a href="/place">處室位置</a></li>
-                        <li class="active"><a href="/phone">聯絡方式</a></li>
-                      </ul>
-                    </div>
-                </div>  
                     @yield('content')
-                </div>
-        </main>
-                <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-                <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script> 
+        </div>
+    </div>
+
     </body>
 </html>
